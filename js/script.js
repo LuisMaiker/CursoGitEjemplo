85fc49e71eb630eedb0999b79cb5e075ede7cb46
luis <luismaiker0295@gmail.com>
2022-11-17 18:45:40 -0400

    actualize el JS y el CSS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-document.write("Esta es una pagina web monitoreada por gitHub");
-document.write(" ");
-document.write("Agregar mas elementos");
-
 var a = 1;
 var b = 2;
-var c = a+b;
+var c = a + b;
 
-documente.write("si la variable a vale " + a " y la variable b vale " + b + " entonces la variable c vale " + c);
+document.write(" "); 
+document.write("Si la variable a vale " + a + " y la variable b vale "+ b + " entonces el valor de c es " + c);
