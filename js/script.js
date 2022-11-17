0686dab2d2148a79eccae7fcbef675e2558d27e0
LuisMaiker <113267656+LuisMaiker@users.noreply.github.com>
2022-11-17 18:28:39 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 document.write("Esta es una pagina web monitoreada por gitHub");
 document.write(" ");
 document.write("Agregar mas elementos");
+
+var a = 1;
+var b = 2;
+var c = a+b;
+
+documente.write("si la variable a vale " + a " y la variable b vale " + b + " entonces la variable c vale " + c);
